0f4c2e1f3c470192414e4f343d17f0d4cbeeffe0
ahmedMoussa2020 <ahmed.moussaisman92@gmail.com>
2022-01-06 20:39:13 +0000

    new files added to campX folder

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,27 +1,28 @@
-
-   
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'vagrant',
-  password: '123',
+  user: 'cambrown',
+  password: '1234',
   host: 'localhost',
   database: 'bootcampx'
 });
 
 const cohortName = process.argv[2];
-const numOfRows = process.argv[3];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
 
-pool.query(`
-SELECT s.id, s.name, cohorts.name as cohort
-FROM students s
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${cohortName}%'
-LIMIT ${numOfRows};
-`)
-  .then(res => {
-    res.rows.forEach(user => {
-      console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
-    });
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+
+pool.query(queryString, values)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
-  .catch(err => console.error('query error', err.stack));
+}).catch(err => console.error('query error', err.stack));
---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,30 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'cambrown',
+  password: '1234',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const cohortName = process.argv[2] || 'JUL02';
+const limit = process.argv[3] || 2;
+const values = [`%${cohortName}%`, limit];
+
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1 
+ORDER BY teacher
+LIMIT $2;
+`;
+
+pool.query(queryString, values)
+.then(res => {
+  console.log(res)
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);  })
+}).catch(err => console.error('query error', err.stack));
